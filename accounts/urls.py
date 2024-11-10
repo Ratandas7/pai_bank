3e80a1586ac408429eea16a456f109294f9c4650
Ratandas7 <ratandas7720@gmail.com>
2024-11-10 10:40:50 +0600

    password change form

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from accounts.views import UserRegistrationView, UserLoginView, UserLogoutView, UserProfileUpdateView
+from accounts.views import UserRegistrationView, UserLoginView, UserLogoutView, UserProfileUpdateView, ChangePasswordView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
     path('profile/', UserProfileUpdateView.as_view(), name='profile'),
+    path('change_password/', ChangePasswordView.as_view(), name='change_password'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,21 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.views import View
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
+
+
+
+def send_account_email(user, subject, template):
+    message = render_to_string(template, {
+        'user' : user
+    })
+    send_email = EmailMultiAlternatives(subject, '', to=[user.email])
+    send_email.attach_alternative(message, "text/html")
+    send_email.send()
 
 # Create your views here.
 class UserRegistrationView(FormView):
@@ -60,3 +75,16 @@ class UserProfileUpdateView(View):
             return redirect('profile')
         return render(request, self.template_name, {'form' : form})
     
+
+
+class ChangePasswordView(LoginRequiredMixin, PasswordChangeView):
+    template_name = 'accounts/change_password.html'
+    success_url = reverse_lazy('profile')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Password has been changed successfully!')
+
+        send_account_email(self.request.user, "Password Changed", "accounts/change_password_email.html")
+
+        return super().form_valid(form)
+  
